4b7dbfe85acd6446d91cd466a56dee9eca4ecbb6
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2024-09-26 02:21:27 -0500

    Update MoviePage - Add file CreateMediaPage

--- frontend/src/components/pages/MoviesPages.js
+++ frontend/src/components/pages/MoviesPages.js
@@ -1,54 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { getAllMedia, createMedia } from '../services/mediaService'
+import { getAllMedia } from '../services/mediaService'
+import CreateMediaPage from './CreateMediaPage';
 
-const MoviesPage = ({media}) => {
+const MoviesPage = ({ media }) => {
     const [mediaList, setMediaList] = useState([])
-    const [newMedia, setNewMedia] = useState({
-        Serial: '',
-        Titulo: '',
-        Sinopsis: '',
-        Url: '',
-        Imagen: '',
-        AnoEstreno: '',
-        Genero: '',
-        Director: '',
-        Productora: '',
-        Tipo: ''
-    })
-
     const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         fetchMedia()
     }, [])
 
-    const handleCreateMedia = async () =>{
-        await createMedia(newMedia)
-        fetchMedia()
-        setShowModal(false)
-        setNewMedia({
-            Serial: '',
-            Titulo: '',
-            Sinopsis: '',
-            Url: '',
-            Imagen: '',
-            AnoEstreno: '',
-            Genero: '',
-            Director: '',
-            Productora: '',
-            Tipo: ''
-        })
-    }
-
-    const handleInputChange = (e)=>{
-        const {name, value} = e.target
-        setNewMedia({...newMedia,[name]:value})
-    }
-
     const fetchMedia = async () => {
         try {
             const data = await getAllMedia()
-            console.log(data);
             if (Array.isArray(data)) {
                 setMediaList(data)
             } else {
@@ -69,188 +33,41 @@ const MoviesPage = ({media}) => {
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
-            timeZone: 'UTC' // Asegura que se formatee en UTC, ajusta según necesidad
+            timeZone: 'America/Bogota'
         }).format(new Date(fecha));
     }
 
     return (
         <div className="container mt-4">
             <h1>Lista de Películas</h1>
-            <button className="btn btn-primary" onClick={() => setShowModal(true)}>
+            <button className="btn btn-primary m-2" onClick={() => setShowModal(true)}>
                 Crear Nueva Película
-            </button>
-            
+            </button>
+
             {mediaList.map((media) => (
-                <>
-                    <div className="card mb-3 border-0 w-100" style={{ maxwidth: '440px' }}>
-                        <div className="row g-0">
-                            <div className="col-md-4">
-                                <img src={media.Imagen} className="img-fluid rounded-start w-100" alt={media.Titulo} />
 
-                            </div>
-                            <div className="col-md-6">
-                                <div className="card-body">
-                                    <h5 className="card-title">{media.Titulo}</h5>
-                                    <p className="card-text">{media.Sinopsis}</p>
-                                    <p className="card-text"><small className="text-body-secondary">
-                                        {media.createdAt ? formatearFecha(media.createdAt) : 'Fecha no disponible'}
-                                    </small></p>
-                                </div>
+                <div key={media.id} className="card mb-3 border-0 w-100" style={{ maxwidth: '440px' }}>
 
-                            </div>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={media.Imagen} className="img-fluid rounded-start w-100" alt={media.Titulo} />
                         </div>
-                    </div>
+                        <div className="col-md-6">
+                            <div className="card-body">
+                                <h5 className="card-title">{media.Titulo}</h5>
+                                <p className="card-text">{media.Sinopsis}</p>
+                                <p className="card-text"><small className="text-body-secondary">
+                                    {media.createdAt ? formatearFecha(media.createdAt) : 'Fecha no disponible'}
+                                </small></p>
+                            </div>
 
-
-                </>
-            ))}
-            {showModal && (
-                <div className="modal fade show d-block" tabIndex="-1" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Crear Nueva Película</h5>
-                                <button type="button" className="btn-close" onClick={() => setShowModal(false)}></button>
-                            </div>
-                            <div className="modal-body">
-                                {/* Campos del formulario */}
-                                <div className="mb-3">
-                                    <label htmlFor="Serial" className="form-label">Serial</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Serial"
-                                        name="Serial"
-                                        value={newMedia.Serial}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Titulo" className="form-label">Título</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Titulo"
-                                        name="Titulo"
-                                        value={newMedia.Titulo}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Sinopsis" className="form-label">Sinopsis</label>
-                                    <textarea
-                                        className="form-control"
-                                        id="Sinopsis"
-                                        name="Sinopsis"
-                                        value={newMedia.Sinopsis}
-                                        onChange={handleInputChange}
-                                        required
-                                    ></textarea>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Url" className="form-label">Url</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Url"
-                                        name="Url"
-                                        value={newMedia.Url}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Imagen" className="form-label">Imagen</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Imagen"
-                                        name="Imagen"
-                                        value={newMedia.Imagen}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="AnoEstreno" className="form-label">Año de Estreno</label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        id="AnoEstreno"
-                                        name="AnoEstreno"
-                                        value={newMedia.AnoEstreno}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                {/* Relaciones */}
-                                <div className="mb-3">
-                                    <label htmlFor="Genero" className="form-label">Género</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Genero"
-                                        name="Genero"
-                                        value={newMedia.Genero}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Director" className="form-label">Director</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Director"
-                                        name="Director"
-                                        value={newMedia.Director}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Productora" className="form-label">Productora</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Productora"
-                                        name="Productora"
-                                        value={newMedia.Productora}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Tipo" className="form-label">Tipo</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="Tipo"
-                                        name="Tipo"
-                                        value={newMedia.Tipo}
-                                        onChange={handleInputChange}
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" onClick={() => setShowModal(false)}>
-                                    Cancelar
-                                </button>
-                                <button type="button" className="btn btn-primary" onClick={handleCreateMedia}>
-                                    Crear
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
-            )}
-        
-       </div>
-
-);
+            ))}
+            {showModal && <CreateMediaPage onClose={() => setShowModal(false)} />}
+        </div>
+    );
 }
 
 export default MoviesPage
--- frontend/src/components/services/mediaService.js
+++ frontend/src/components/services/mediaService.js
@@ -6,22 +6,58 @@ export const getAllMedia = async () => {
         const data = await response.json()
         return data
     } catch (error) {
-        console.error("Error al obtener las peliculas: ",error);
-        return[]
+        console.error("Error al obtener las peliculas: ", error);
+        return []
     }
 }
 
+export const getFormData = async () => {
+    try {
+        const [generosRes, directoresRes, productorasRes, tiposRes] = await Promise.all([
+            fetch(`${URL_BASE}/genero`),
+            fetch(`${URL_BASE}/director`),
+            fetch(`${URL_BASE}/productora`),
+            fetch(`${URL_BASE}/tipos`),
+        ]);
+
+        const generos = await generosRes.json();
+        const directores = await directoresRes.json();
+        const productoras = await productorasRes.json();
+        const tipos = await tiposRes.json();
+
+        return {
+            generos,
+            directores,
+            productoras,
+            tipos
+        };
+    } catch (error) {
+        console.error('Error fetching form data:', error);
+        return null;
+    }
+};
+
+
 export const createMedia = async (media) => {
     try {
-        const response = await fetch(`${URL_BASE}/media/create`,{
-            method:'POST',
-            //headers:{'Content-Type':'aplication/json'},
-            body:JSON.stringify(media)
+
+        const response = await fetch(`${URL_BASE}/media/create`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(media),
         })
+
+
+        if (!response.ok) {
+            const errorMessage = await response.json();
+            console.error('Error desde el servidor:', errorMessage); 
+            throw new Error(errorMessage.message || 'Error al crear la media');
+        }
+
         const data = await response.json()
         return data
     } catch (error) {
-        console.error("Error al crear la pelicula:",error);
-        return[]        
+        console.error('Error al crear la media:', error.response ? error.response.data : error.message);
+        throw error;
     }
 }
