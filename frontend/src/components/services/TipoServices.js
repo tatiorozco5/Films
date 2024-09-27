4fcf7b04a2bc2913f9acbcb57661fe322f35ad75
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2024-09-27 10:02:02 -0500

    creation functionality in TypePage

--- frontend/src/components/services/TipoServices.js
+++ frontend/src/components/services/TipoServices.js
@@ -6,10 +6,37 @@ export const getAllTipos = async () => {
         const response = await fetch(`${URL_BASE}/tipos`)
         const data = await response.json()
         console.log(data);
-        
+
         return data
     } catch (error) {
         console.error("Error al obtener las peliculas: ", error);
         return []
+    }
+}
+
+
+
+export const CreateTipo = async (newTipo) => {
+    try {
+        const response = await fetch(`${URL_BASE}/tipos/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newTipo)
+        })
+
+        if (!response.ok) {
+            const MessageError = await response.json()
+            console.log("Error al obtener los tipos: ", MessageError);
+            throw new Error(MessageError.error)
+
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error('Error al crear el tipo:', error.response ? error.response.data : error.message);
+        throw error;
     }
 }
